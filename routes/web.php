d374fadc1e5f0ac7eb7d1c62b4eff6c953bdbfb3
banjoDevs <pableobenedick033@gmail.com>
2023-02-19 13:41:52 +0800

    fixing routes

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::resource('posts', PostController::class);
     // Route::get('posts/posted', [PostedController::class, 'posts'])->name('posted');
-     Route::resource('posts', PostController::class);
     // Route::get('/', [PostController::class, 'index'])->name('home');
 });
 
