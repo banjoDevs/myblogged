9ab92af30d2024dfde58bb016cb6760d2a2f46af
banjoDevs <pableobenedick033@gmail.com>
2023-02-19 09:53:19 +0800

    controllers for my auth and registration

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+}
