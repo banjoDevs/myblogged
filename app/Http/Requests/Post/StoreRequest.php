ad3521bd3737c24d8229bd25d13c252ce9eaa2cb
banjoDevs <pableobenedick033@gmail.com>
2023-02-19 14:25:19 +0800

    config max kb

--- app/Http/Requests/Post/StoreRequest.php
+++ app/Http/Requests/Post/StoreRequest.php
@@ -24,7 +24,7 @@ class StoreRequest extends FormRequest
         return [
             'title' => 'required|string|min:3|max:250',
             'content' => 'required|string|min:3|max:6000',
-            'featured_image' => 'required|image|max:100000|mimes:jpg,jpeg,png',
+            'featured_image' => 'required|image|max:10000|mimes:jpg,jpeg,png',
         ];
     }
 }
